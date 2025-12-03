c4b6e01f1acf2a21d6a86324154bda096ce36417
mittalshivam226 <mittalshivam226@gmail.com>
2025-12-03 22:57:02 +0530

    Update main.ts

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -21,8 +21,8 @@ async function bootstrap() {
     .addTag('users')
     .addTag('listings')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument(app as any, config);
+  SwaggerModule.setup('api', app as any, document);
 
   await app.listen(3001);
 }
